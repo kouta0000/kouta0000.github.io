c3466fbc45892595d650d3dbb8e6682d4271fbd5
kota <kukkmathphys1236@gmail.com>
2025-10-09 13:55:10 +0900

    a

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -74,7 +74,7 @@ const url = `${base}search-index.json`;
     };
   </script>
   
-  <div class="w-full max-w-md mx-auto relative group sticky top-10 mb-6">
+  <div class="w-full max-w-md mx-auto relative group fixed top-10 mb-6">
     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
       Search
     </label>
