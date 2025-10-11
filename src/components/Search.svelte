93482a8633c79558485d18a28dc3a370544536ee
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2025-10-11 11:35:46 +0900

    Update Search.svelte

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -92,7 +92,7 @@ const url = `${base}search-index.json`;
         bind:value={query}
         oninput={()=>handleQuery(query)}
         onfocusout={() => { setTimeout(()=>{results = []; query = '';show=false},300)}}
-        class="shadow-md rounded-full block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="shadow-md rounded-xl block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Search"
         required
       />
