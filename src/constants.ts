2c9794f7f79c086125c5e699148120ae1f9e10da
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2026-03-31 02:12:27 +0900

    Update email address in constants

--- src/constants.ts
+++ src/constants.ts
@@ -37,7 +37,7 @@ export const SOCIALS: Social[] = [
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:kukkmathphys1236@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
