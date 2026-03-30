1595c1335990408419dda0c6e925732487baa83b
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2026-03-31 01:30:36 +0900

    Remove trailing slash from base URL in config

--- astro.config.ts
+++ astro.config.ts
@@ -14,7 +14,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: '/myblog/',
+  base: '/myblog',
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
