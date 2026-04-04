1d9032f374ef9c5eb8a7a190a7e7d4fcc69b1604
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2026-04-05 03:20:08 +0900

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -15,7 +15,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // 2. static から server に変更
+  output: 'hybrid', // 2. static から server に変更
   adapter: netlify(),
   site: SITE.website, // サブディレクトリを含めないドメインのみ
   integrations: [
