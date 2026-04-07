2136836bb25ff6a8dd00204066079b6acd377399
kouta0000 <39004878+kouta0000@users.noreply.github.com>
2026-04-07 17:05:41 +0900

    Update config.ts

--- db/config.ts
+++ db/config.ts
@@ -1,26 +1,26 @@
 // db/config.ts
 import { defineDb, defineTable, column } from "astro:db";
 
-const Comment = defineTable({
+export const Comment = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
     postSlug: column.text(),
     author: column.text(),
     email: column.text({ optional: true }),
     body: column.text(),
-    createdAt: column.date({ default: new Date() }),
+    createdAt: column.date({ default: () => new Date() }),
     approved: column.boolean({ default: true }),
   },
   indexes: [
     { on: ["postSlug"] },
   ],
 });
-const BoardPost = defineTable({
+export const BoardPost = defineTable({
   columns: {
     id: column.number({ primaryKey: true }),
     name: column.text({ default: "Anonymous" }),
     body: column.text(),
-    postedAt: column.date({ default: new Date() }),
+    postedAt: column.date({ default: () => new Date() }),
   },
 });
 export default defineDb({
